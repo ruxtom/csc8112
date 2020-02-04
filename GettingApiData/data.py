e8a529fe7c7eb44124930666701681c5cf2799c1
MRodrick <32981608+mrodrick@users.noreply.github.com>
2020-02-04 15:43:12 +0000

    Added times to run, split code into methods for easier readability

--- GettingApiData/data.py
+++ GettingApiData/data.py
@@ -1,20 +1,38 @@
 import json, requests
 
-url = requests.get('https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity')
 
-info = json.loads(url.text)
-z = info
-tempSensors = []
-data = []
-for i in z["items"]:
-	for x in i["feed"]:
-		if x["metric"] == "Room Temperature":
-			tempSensors.append(x)
 
-for a in tempSensors:
-	for b in a["timeseries"]:
-		value = b["latest"]["value"]
-		data.append(value)
+timesToRun = 0
+timesToRun = input("How many times to run data collection?")
+time = 0
 
-print (data)
+def main():
+	global time, timesToRun
+	print("in main")
+	a = int(timesToRun)
+	url = requests.get('https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity')
+	info = json.loads(url.text)
+	if time < a:
+		time = time + 1
+		temperature(info)
+
+
+
+def temperature(z):
+	tempSensors = []
+	data = []
+	for i in z["items"]:
+		for x in i["feed"]:
+			if x["metric"] == "Room Temperature":
+				tempSensors.append(x)
+
+	for a in tempSensors:
+		for b in a["timeseries"]:
+			value = b["latest"]["value"]
+			data.append(value)
+
+	print (data)
+	main()
+
 #print (info)
+main()
