b8b5db70a570d61f2966be51273dc08ec3d54a4c
MRodrick <m.rodrick@newcastle.ac.uk>
2020-02-06 20:05:11 +0000

    making list of sensors with vals

--- GettingApiData/data.py
+++ GettingApiData/data.py
@@ -10,7 +10,7 @@ def main():
 	global time, timesToRun
 	print("in main")
 	a = int(timesToRun)
-	url = requests.get('https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity')
+	url = requests.get('https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity/floor-1')
 	print("after url")
 	info = json.loads(url.text)
 	if time < a:
@@ -23,12 +23,13 @@ def temperature(z):
 	if z != None:
 		tempSensors = []
 		data = []
-		for i in z['items']:
-			for x in i["feed"]:
-				if x["metric"] == "Room Temperature":
-					tempSensors.append(x)
+		#for i in z["items"]:
+		for x in z["feed"]:
+			if x["metric"] == "C3 HTG Pump Power":
+				tempSensors.append(x)
 
 	for a in tempSensors:
+		#print(a)
 		for b in a["timeseries"]:
 			value = b["latest"]["value"]
 			data.append(value)
