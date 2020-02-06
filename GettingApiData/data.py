58e66d5b1c24d0dc74e43be45872258b071616be
MRodrick <m.rodrick@newcastle.ac.uk>
2020-02-06 09:10:56 +0000

    Code in methods, select number of times to collect temp data

--- GettingApiData/data.py
+++ GettingApiData/data.py
@@ -11,6 +11,7 @@ def main():
 	print("in main")
 	a = int(timesToRun)
 	url = requests.get('https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity')
+	print("after url")
 	info = json.loads(url.text)
 	if time < a:
 		time = time + 1
@@ -19,12 +20,13 @@ def main():
 
 
 def temperature(z):
-	tempSensors = []
-	data = []
-	for i in z["items"]:
-		for x in i["feed"]:
-			if x["metric"] == "Room Temperature":
-				tempSensors.append(x)
+	if z != None:
+		tempSensors = []
+		data = []
+		for i in z['items']:
+			for x in i["feed"]:
+				if x["metric"] == "Room Temperature":
+					tempSensors.append(x)
 
 	for a in tempSensors:
 		for b in a["timeseries"]:
