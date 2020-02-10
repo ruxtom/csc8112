bd6a93e9a551d0ab8754f76af4499d3fe062e5c8
MRodrick <32981608+MRodrick@users.noreply.github.com>
2020-02-10 13:54:08 +0000

    Can determine all sensors on each /entity/floor, and all sensors shown in /entity

--- GettingApiData/findingEntitiesWithValue.py
+++ GettingApiData/findingEntitiesWithValue.py
@@ -5,26 +5,49 @@ import time
 
 
 # Gets values from the API a specified number of times, waiting 5 seconds between each request
-def main():
+def floorSensors():
     maxFloors = 6
     i = 1
     while i <= maxFloors:
         url = requests.get("https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity/floor-"+str(i))
         info = json.loads(url.text)
-        determineIfHasValue(info)
+        print("Entity sensors for floor " + str(i))
+        determineSensorsByFloor(info)
         i+= 1
 
-def determineIfHasValue(info):
+def entitySensors():
+    url = requests.get("https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity")
+    info = json.loads(url.text)
+    determineAllSensors(info)
+
+
+def determineAllSensors(info):
+    hasValue = []
+    if info is None:
+        return
+    for items in info["items"]:
+        for feed in items["feed"]:
+            for timeseries in feed["timeseries"]:
+                if "latest" in timeseries:
+                    name = feed["metric"]
+                    hasValue.append(name)
+    counter = collections.Counter(hasValue)
+    print("All entity sensors:")
+    print(counter)
+
+def determineSensorsByFloor(info):
     hasValue = []
     if info is None:
         return
     for feed in info["feed"]:
         for timeseries in feed["timeseries"]:
             if "latest" in timeseries:
-                # print(name)
                 name = feed["metric"]
                 hasValue.append(name)
     counter = collections.Counter(hasValue)
     print(counter)
 
-main()
+
+entitySensors()
+floorSensors()
+
