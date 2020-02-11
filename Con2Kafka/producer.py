e507e370cdf4e7c483bd58e7f6675f016a53e06a
jacob0522 <60758925+jacob0522@users.noreply.github.com>
2020-02-11 23:13:09 +0000

    Add files via upload

---
+++ Con2Kafka/producer.py
@@ -0,0 +1,24 @@
+import json
+from kafka import KafkaProducer
+
+
+def send2aws():
+    producer = KafkaProducer(
+        value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+        bootstrap_servers=['ec2-18-207-191-122.compute-1.amazonaws.com:9092',
+                           'ec2-3-81-136-58.compute-1.amazonaws.com:9092',
+                           'ec2-54-196-123-115.compute-1.amazonaws.com:9092']
+    )
+    for i in range(10):
+        data = {
+            "device": "temperature",
+            "tem": 110,
+            "room": i
+        }
+        print(data)
+        producer.send('usb', data)
+    producer.close()
+
+
+if __name__ == '__main__':
+    send2aws()
