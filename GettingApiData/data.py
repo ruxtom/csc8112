6594db5d8c3a90ee933acaedcee7161425752913
MRodrick <m.rodrick@newcastle.ac.uk>
2020-02-12 15:04:59 +0000

    Getting all temperature sensors of all rooms

--- GettingApiData/data.py
+++ GettingApiData/data.py
@@ -4,43 +4,56 @@ import collections
 import time
 import json
 from kafka import KafkaProducer
-import data
 
-
+failedSensors = 0
 # Gets values from the API a specified number of times, waiting 5 seconds between each request
 def main():
-    producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'),bootstrap_servers=['ec2-18-207-191-122.compute-1.amazonaws.com:9092','ec2-3-81-136-58.compute-1.amazonaws.com:9092','ec2-54-196-123-115.compute-1.amazonaws.com:9092'])
-    #totalIterations = int(input("How many times would you like to collect the data: "))
+    producer = KafkaProducer(
+        value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+        bootstrap_servers=['ec2-18-207-191-122.compute-1.amazonaws.com:9092',
+                           'ec2-3-81-136-58.compute-1.amazonaws.com:9092',
+                           'ec2-54-196-123-115.compute-1.amazonaws.com:9092']
+    )
+#totalIterations = int(input("How many times would you like to collect the data: "))
     #if type(totalIterations) is not int:
     #    raise Exception("Invalid total iterations type")
     completedIterations = 0
 
     #while completedIterations < totalIterations:
-    url = requests.get("https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity/floor-1")
-    info = json.loads(url.text)
-    temperature(info)
-    completedIterations += 1
+    #url = requests.get("https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity/floor-1")
+    #url = requests.get('https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity?metric="room temperature"')
+    i = 1
+    for i in range(41):
+        url = requests.get('https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity?metric="room temperature"&page='+str(i))
+        info = json.loads(url.text)
+        temperature(info)
+        completedIterations += 1
     #time.sleep(5)
     #print("Iteration: ", completedIterations, "/", totalIterations)
     #print("Time left: ", (totalIterations - completedIterations)*5, "s")
-    #send = {
-    #    "data":json.dumps(temperature(info))
-    #}
-    #print(send)
-    #producer.send('usb-data', send)
-    #producer.close()
-    print("After producer close")
-
+        toSend = temperature(info)
+        c = 0
+        length = len(toSend)
+        print("Sending data...")
+        for c in range(length):
+            send = {
+                "data":json.dumps(toSend[c])
+            }
+            producer.send('usb', send)
+            producer.flush()
+    producer.close()
+    print("Unusable sensors: " + str(failedSensors))
 
 # Extracts the temperature values from the API information
 def temperature(info):
-    data = _data_extractor(info, "Power")
+    data = _data_extractor(info, "Temperature")
     #print(data)
     return data
 
 
 # Loops through the info array, extracting the specified metric values
 def _data_extractor(info, metricWanted):
+    global failedSensors
     sensors = []
     metrics = []
     data = []
@@ -48,25 +61,38 @@ def _data_extractor(info, metricWanted):
     if info is None:
         return
         
-    for feed in info["feed"]:
-        metric = feed["metric"]
-        if metricWanted in metric:
-            if metric != "Power":
-                if "Voltage" not in metric:
-                    sensors.append(feed)
-                    metrics.append(metric)
+    for items in info["items"]:
+        name = items["name"]
+        for feed in items["feed"]:
+            metric = feed["metric"]
+            if metricWanted in metric:
+                for timeseries in feed["timeseries"]:
+                    if "latest" in timeseries:
+                        try:
+                            value = timeseries["latest"]["value"]
+                        except:
+                            failedSensors = failedSensors + 1
+                            print("An Error in last timeseries")
+                        try:
+                            whole.append(name +" " + str(value))
+                        except:
+                            print("Can't append whole")
+                #if metric != "Power":
+                    #if "Voltage" not in metric:
+                #sensors.append(feed)
+                #metrics.append(metric)
 
-    for sensor in sensors:
-        name = sensor["metric"]
-        for timeseries in sensor["timeseries"]:
-            if "latest" in timeseries:
-                
-                value = timeseries["latest"]["value"]
-                whole.append(name +" " + str(value))
-                
-                data.append(value)
-
-    print(whole)
-    return data
+    #for sensor in sensors:
+    #    sType = sensor["metric"]
+    #    name = sensor["name"]
+    #    for timeseries in sensor["timeseries"]:
+    #        if "latest" in timeseries:
+    #            
+    #            value = timeseries["latest"]["value"]
+    #            whole.append(sType + " " + name +" " + str(value))
+    #            
+    #            data.append(value)
+    #print(whole)
+    return whole
 
 main()
