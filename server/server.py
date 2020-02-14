2d7b5b6d7e37beed130c68fe46889f2af7dea774
Tom Ruxton <ruxton.tom@gmail.com>
2020-02-14 12:04:08 +0000

    Added Flask server to serve static html files Created a placeholder index with styling

---
+++ server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='public')
+
+@app.route("/")
+def hello():
+    return "Welcome to the visualiser! Navigate to /index.html to view visualisations."
+
+if __name__ == "__main__":
+	app.run()
