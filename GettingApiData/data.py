108e5749687e34010470a529bc8a55270ed07c0e
MRodrick <m.rodrick@newcastle.ac.uk>
2020-02-14 15:54:27 +0000

    Removed Con2Kafka

--- GettingApiData/data.py
+++ GettingApiData/data.py
@@ -10,40 +10,42 @@ failedSensors = 0
 def main():
     producer = KafkaProducer(
         value_serializer=lambda v: json.dumps(v).encode('utf-8'),
-        bootstrap_servers=['ec2-18-207-191-122.compute-1.amazonaws.com:9092',
-                           'ec2-3-81-136-58.compute-1.amazonaws.com:9092',
-                           'ec2-54-196-123-115.compute-1.amazonaws.com:9092']
+        bootstrap_servers=['ec2-34-228-191-220.compute-1.amazonaws.com:9092',
+                           'ec2-52-23-217-206.compute-1.amazonaws.com:9092',
+                           'ec2-54-152-219-229.compute-1.amazonaws.com:9092']
     )
-#totalIterations = int(input("How many times would you like to collect the data: "))
-    #if type(totalIterations) is not int:
-    #    raise Exception("Invalid total iterations type")
+    totalIterations = int(input("How many times would you like to collect the data: "))
+    if type(totalIterations) is not int:
+        raise Exception("Invalid total iterations type")
     completedIterations = 0
 
-    #while completedIterations < totalIterations:
+    while completedIterations < totalIterations:
+        failedSensors = 0
+        completedIterations += 1
     #url = requests.get("https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity/floor-1")
     #url = requests.get('https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity?metric="room temperature"')
-    i = 1
-    for i in range(41):
-        url = requests.get('https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity?metric="room temperature"&page='+str(i))
-        info = json.loads(url.text)
-        temperature(info)
-        completedIterations += 1
-    #time.sleep(5)
-    #print("Iteration: ", completedIterations, "/", totalIterations)
-    #print("Time left: ", (totalIterations - completedIterations)*5, "s")
-        toSend = temperature(info)
-        c = 0
-        length = len(toSend)
-        print("Sending data...")
-        for c in range(length):
-            send = {
-                "data":json.dumps(toSend[c])
-            }
-            producer.send('usb', send)
-            producer.flush()
-    producer.close()
-    print("Unusable sensors: " + str(failedSensors))
-
+        i = 1
+        for i in range(41):
+            url = requests.get('https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/entity?metric="room temperature"&page='+str(i))
+            info = json.loads(url.text)
+            temperature(info)
+            
+        #time.sleep(5)
+        #print("Iteration: ", completedIterations, "/", totalIterations)
+        #print("Time left: ", (totalIterations - completedIterations)*5, "s")
+            toSend = temperature(info)
+            c = 0
+            length = len(toSend)
+            print("Sending data...")
+            for c in range(length):
+                send = {
+                    "data":json.dumps(toSend[c])
+                }
+                producer.send('usb', send)
+                producer.flush()
+        producer.close()
+        print("Unusable sensors: " + str(failedSensors))
+   
 # Extracts the temperature values from the API information
 def temperature(info):
     data = _data_extractor(info, "Temperature")
@@ -92,7 +94,7 @@ def _data_extractor(info, metricWanted):
     #            whole.append(sType + " " + name +" " + str(value))
     #            
     #            data.append(value)
-    #print(whole)
+    print(whole)
     return whole
 
 main()
