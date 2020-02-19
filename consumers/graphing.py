1aad9f1ee5734956ab7f582520796a18d078412e
Tom Ruxton <ruxton.tom@gmail.com>
2020-02-19 14:09:54 +0000

    Added graph performance evaluation

--- consumers/graphing.py
+++ consumers/graphing.py
@@ -1,7 +1,11 @@
 import plotly.graph_objects as go
+from time import process_time
 
 
 def createGraph(title, xAxis, yAxis, outputFileName):
+    tStart = process_time()
     fig = go.Figure(data=go.Bar(x=xAxis, y=yAxis))
     fig.write_html("./server/public/external_html/" + str(outputFileName) +
                    ".html")
+    tStop = process_time()
+    print("Total graphing time:", tStop-tStart)
