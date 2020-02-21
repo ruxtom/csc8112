af0c38ba0cb0be575f3b272203f44bdf5012c48e
Tom Ruxton <ruxton.tom@gmail.com>
2020-02-21 15:14:29 +0000

    fix graph

--- consumers/graphing.py
+++ consumers/graphing.py
@@ -6,7 +6,9 @@ def createGraph(title, xAxis, yAxis, yAxisTitle, outputFileName):
     tStart = process_time()
     fig = go.Figure(data=go.Bar(x=xAxis, y=yAxis))
     fig.update_layout(title=title, xaxis_title="Room", yaxis_title=yAxisTitle)
-    fig.write_html("./server/public/external_html/" + str(outputFileName) +
+    fig.write_html("../server/public/external_html/" + str(outputFileName) +
                    ".html")
+    # fig.write_html("./server/public/external_html/" + str(outputFileName) +
+    #                ".html")
     tStop = process_time()
     print("Total graphing time:", tStop-tStart)
